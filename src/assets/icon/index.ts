7598358d912eae4053f03d3aed00637249c4c4de
José Berríos <surphury@gmail.com>
2022-08-04 09:10:17 -0400

    feat: add assets

---
+++ src/assets/icon/index.ts
@@ -0,0 +1,9 @@
+import clock from './clock.svg';
+import solidHeart from './solid-heart.svg';
+import heart from './heart.svg';
+import angular from './angular.svg';
+import react from './react.svg';
+import vue from './vue.svg';
+import arrow from './arrow.svg';
+
+export { clock, heart, solidHeart, angular, react, vue, arrow };

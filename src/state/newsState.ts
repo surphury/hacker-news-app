6110a7726235edb1d3088ad55fa6602f6779d20b
José Berríos <surphury@gmail.com>
2022-08-06 21:21:12 -0400

    refactor: add comments

--- src/state/newsState.ts
+++ src/state/newsState.ts
@@ -5,16 +5,21 @@ import { LocalNews } from '@/models';
 
 import { getNews } from '@/services';
 
-import { PostsState } from '@/state';
+import { topicState } from '@/state';
+
+import { getFromLocalStorage } from '@/utils';
+
+import { LocalKeys } from '@/config';
 
 export const localNews = atom(async (get) => {
-	const news = localStorage.getItem('news');
-
 	/* if the post isn't local posts and selected posts are not the same, this condition will be false */
-	if (news && (JSON.parse(news) as LocalNews).from === get(PostsState)) {
-		return JSON.parse(news) as LocalNews;
+	const news = getFromLocalStorage<LocalNews>(LocalKeys.News);
+	/* check if it is true and if we have the needed news for example: 
+	This state will update change from react to angular's news */
+	if (news?.from === get(topicState)) {
+		return news;
 	} else {
-		const res = await getNews(get(PostsState), 0);
+		const res = await getNews(get(topicState), 0);
 
 		localStorage.setItem('news', JSON.stringify(res));
 

675cc7319211d5505979c1d6b2e9430ae90328b5
José Berríos <surphury@gmail.com>
2022-08-06 21:15:31 -0400

    refactor: declare keys as enums

---
+++ src/config/LocalKeys.ts
@@ -0,0 +1,4 @@
+export enum LocalKeys {
+	Favorites = 'favorites',
+	News = 'news'
+}
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,4 @@
 export { Routing } from './Routing';
 export { Api } from './Api';
 export { Post } from './Post';
+export { LocalKeys } from './LocalKeys';
